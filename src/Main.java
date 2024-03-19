eb1d042bd17f6dfe96ff33366cf4b9206d5dd7c6
hector_salamanca <hector@interlinked.168.1.6>
2024-03-19 17:40:27 +0800

    added refresh method

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,38 @@ import java.io.FileReader;
 
 public class Main {
 
-    public String employee_hourly_rate = "";
-    public String employee[];
+    // Fields
+    public static String employee_hourly_rate;
+    public static String employee[];
 
     public static void main(String[] args) {
 
+
+    }
+
+    // This method refreshed the field's data
+    public static void refreshData() {
+        employee_hourly_rate = "";
+        employee = new String[0];
+    }
+
+    public static String getEmployeeDetails(String employee_id) {
+
+        String file = System.getProperty("user.dir") + "/src/Employee_Details.csv";
+
+        BufferedReader reader = null;
+        String line = "";
+        String employee_found = "";
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            while( (line = reader.readLine()) != null) {
+                String repl = line.replaceAll(",(?!(([^\"]*\"){2})*[^\"]*$)", ";x;");
+                String[] row = repl.split(",");
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
