51fb8958096c7f64ce6f573ce99985881d67a65e
hector_salamanca <hector@interlinked.168.1.6>
2024-03-19 17:28:53 +0800

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
